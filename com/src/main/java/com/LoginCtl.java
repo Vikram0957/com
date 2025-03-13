983bfe35a451c4023c5f69613915a82ae19da2ef
Vikram0957 <vikram.singh6822@gmail.com>
2025-03-13 10:28:27 +0530

    Update LoginCtl.java

--- com/src/main/java/com/LoginCtl.java
+++ com/src/main/java/com/LoginCtl.java
@@ -19,9 +19,6 @@ public class LoginCtl extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<h4>This is LoginCtl Controller</h4>");
 
-		
-		
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
